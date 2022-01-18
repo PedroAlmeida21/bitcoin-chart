8f7068600168368dfb9d3309dfa330f9d9dc6ac7
EMEA\Pedro.AlmeidaDaSilva <pedro.almeidadasilva@adeccogroup.com>
2022-01-18 17:52:33 +0100

    created simple navbar

---
+++ src/navbar/index.tsx
@@ -0,0 +1,13 @@
+import './nav-bar.scss';
+
+function NavBar() {
+  return (
+    <header className='navigation-bar'>
+      <div className='container'>
+        <img src="https://www.adecco.cz/wp-content/uploads/2017/02/adecco.png"/>
+      </div>
+    </header>
+  );
+}
+
+export default NavBar;
