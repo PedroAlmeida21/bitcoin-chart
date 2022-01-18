7fe6baf4694abc10c495ea1071a5171084f9bbad
EMEA\Pedro.AlmeidaDaSilva <pedro.almeidadasilva@adeccogroup.com>
2022-01-18 19:10:19 +0100

    changed CDN url for image

--- src/navbar/index.tsx
+++ src/navbar/index.tsx
@@ -4,7 +4,7 @@ function NavBar() {
   return (
     <header className='navigation-bar'>
       <div className='container'>
-        <img src="https://www.adecco.cz/wp-content/uploads/2017/02/adecco.png"/>
+        <img src="https://www.adeccousa.com/-/media/adeccogroup/brands/adecco-global-2016/assets/media/shared/logo/adecco-logo.png?rev=f75ec570534c4bf594fbd8fdccb580a5"/>
       </div>
     </header>
   );
